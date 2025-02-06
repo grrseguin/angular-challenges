0d542a363bd26b7ccb44405bc3ce9453c04620fd
Guillaume Seguin <grrseguin@gmail.com>
2025-02-06 15:14:35 +0100

    feat: add global error handler

---
+++ apps/angular/5-crud-application/src/app/AppErrorHandler.ts
@@ -0,0 +1,8 @@
+import { ErrorHandler, Injectable } from '@angular/core';
+
+@Injectable()
+export class AppErrorHandler implements ErrorHandler {
+  handleError(error: any) {
+    console.error(error);
+  }
+}
--- apps/angular/5-crud-application/src/app/app.config.ts
+++ apps/angular/5-crud-application/src/app/app.config.ts
@@ -1,6 +1,10 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, ErrorHandler } from '@angular/core';
+import { AppErrorHandler } from './AppErrorHandler';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient()],
+  providers: [
+    provideHttpClient(),
+    { provide: ErrorHandler, useClass: AppErrorHandler },
+  ],
 };
