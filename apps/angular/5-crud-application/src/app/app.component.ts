ffad15a4bafaa4b0ee66f42051cea03102e4d32b
Guillaume Seguin <grrseguin@gmail.com>
2025-02-06 14:33:17 +0100

    refactor: add todo model

--- apps/angular/5-crud-application/src/app/app.component.ts
+++ apps/angular/5-crud-application/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { randText } from '@ngneat/falso';
+import { Todo } from './todo.model';
 
 @Component({
   imports: [CommonModule],
@@ -15,26 +16,25 @@ import { randText } from '@ngneat/falso';
   styles: [],
 })
 export class AppComponent implements OnInit {
-  todos!: any[];
+  todos!: Todo[];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http
-      .get<any[]>('https://jsonplaceholder.typicode.com/todos')
+      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
       .subscribe((todos) => {
         this.todos = todos;
       });
   }
 
-  update(todo: any) {
+  update(todo: Todo) {
     this.http
-      .put<any>(
+      .put<Todo>(
         `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
         JSON.stringify({
           todo: todo.id,
           title: randText(),
-          body: todo.body,
           userId: todo.userId,
         }),
         {
@@ -43,7 +43,7 @@ export class AppComponent implements OnInit {
           },
         },
       )
-      .subscribe((todoUpdated: any) => {
+      .subscribe((todoUpdated: Todo) => {
         this.todos[todoUpdated.id - 1] = todoUpdated;
       });
   }
